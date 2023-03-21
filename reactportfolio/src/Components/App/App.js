a18c3d64b6466b203039cd63852e42fb7cde2d66
rory jackson <roryjamesjackson@hotmail.com>
2023-03-21 16:06:44 +0000

    basic set up of files

---
+++ reactportfolio/src/Components/App/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from '../Header/Header';
+import Projects from '../Projects/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      < Header />
+      < Projects />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactportfolio/src/Components/Projects/Projects.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Projects.css';
+
+class Projects extends React.Component {
+  render() {
+    return (
+      <div className='projects-container'>
+        <div className="project">
+          <h2>Project title</h2>
+          <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt=""></img>
+        </div>
+        <div className="project">
+          <h2>Project title</h2>
+          <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt=""></img>
+        </div>
+        <div className="project">
+          <h2>Project title</h2>
+          <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt=""></img>
+        </div>
+        <div className="project">
+          <h2>Project title</h2>
+          <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt=""></img>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default Projects;
